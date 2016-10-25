f95eccb303c2f130656726864556649c32ce975a
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2016-10-25 11:24:07 +0100

    Corrected for demo :-)

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.get('uthtest5', function (req,res) {
 
 app.get('/temenos', function (req,res) {
   res.send('<html><head><title>TEMENOS Example</title></head><body bgcolor="white"><b>EXAMPLE <font color="red">NODE.JS</font> RESPONSE</b></body></html>');
-};
+});
 
 // error handling
 app.use(function(err, req, res, next){
