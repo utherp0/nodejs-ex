c1b4f510fd80cab14c2b78f525cd7151de5fdad7
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2017-03-30 11:49:48 +0100

    AB Test release 1

--- server.js
+++ server.js
@@ -115,6 +115,10 @@ app.get('/temenos', function (req,res) {
   res.send('<html><head><title>TEMENOS Example</title></head><body bgcolor="white"><b>EXAMPLE <font color="red">NODE.JS</font> RESPONSE</b></body></html>');
 });
 
+app.get('abtest', function (req,res) {
+  res.send( '<b>Release 1!</b> ' );
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
