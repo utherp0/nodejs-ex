04fa0365105bd090f6faadd22fccd6fd5b69c02f
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2017-05-16 12:56:55 +0100

    Changed page

--- server.js
+++ server.js
@@ -51,16 +51,18 @@ var initDb = function(callback) {
 };
 
 app.get('/', function (req, res) {
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+  //if (db) {
+  //  var col = db.collection('counts');
+  //  // Create a document with request IP and current time of request
+  //  col.insert({ip: req.ip, date: Date.now()});
+  //  col.count(function(err, count){
+  //    res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+  //  });
+  //} else {
+  //  res.render('index.html', { pageCountMessage : null});
+  //}
+
+  res.render('rhsso_test.html');
 });
 
 app.get('/pagecount', function (req, res) {
